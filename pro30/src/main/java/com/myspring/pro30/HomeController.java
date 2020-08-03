9a6631a5f1cda371b0f3562f63d6ec7e1bdb6aaa
Administrator <Administrator@172.36.133.58>
2020-08-03 20:42:47 +0900

    qq

--- pro30/src/main/java/com/myspring/pro30/HomeController.java
+++ pro30/src/main/java/com/myspring/pro30/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	//@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		System.out.println("섹스섹스");
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
