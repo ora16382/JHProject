12fdf41284c3e16bd45ad53ce16983e101f91780
Administrator <Administrator@172.36.133.58>
2020-08-05 10:17:54 +0900

    8월5일

--- pro30/src/main/java/com/myspring/pro30/HomeController.java
+++ pro30/src/main/java/com/myspring/pro30/HomeController.java
@@ -24,8 +24,8 @@ public class HomeController {
 	 */
 	//@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-	//한글테스트입니다.
-		System.out.println("한글테스트입니다.");
+
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
